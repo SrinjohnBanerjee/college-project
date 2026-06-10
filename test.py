5974d8f01e3f476851b7e88c5a29595d6ee523bd
Srinjohn Banerjee <srinjohnbanerjee07@gmail.com>
2026-06-10 22:12:39 +0530

    Refactor and enhance DNS and form counting functions

--- test.py
+++ test.py
@@ -59,6 +59,45 @@ def formcount(url):
                     return 1
     except:
         return 1
+
+
+
+
+
+
+
+import dns.resolver
+import requests 
+
+def virustotal(url):
+     address=dns.resolver.resolve(url, 'A')
+     api= f"https://www.virustotal.com/api/v3/ip_addresses/{address[0]}"
+     headers={
+        "accept": "application/json",
+        "x-apikey": "9cef20c75ef022621d4437ed41c6bc267e5cf2020b2001c41c3b180020377628"
+     }
+     response = requests.get(api, headers=headers)
+     return response.json()
+
+
+answer=virustotal("google.com")
+print(answer)
+
+i have to get this access 
+'last_https_certificate': {'cert_signature': {'signature_algorithm': 'sha256RSA', 'signature': '1ee8318ada9f55671d0d8ec9d65227f7abf922e98a43a48dd244528dc3c00efa0286cf0c0b29d22889d68e3385b498038ff0121cc9e48eb4ebc69a73a5d4c9c519ad6cc72d6bbac30cb876724487a5997a50c7ea0b9d199e3b850c244ad29b808aeabded3e9ba01ec8c962ed3fc7832dc488281fdde9fdc5d5e9ffc3cc9e44752fb0fab370b7ab2a7299b30ee9867c36df872f1428241f5bdbb3f68952ba4b6c9497e3de4bb919a14a9de10884d5e09fc9a37c22c0206fe96e3866bde33238549648584a367df97d396539f306222137792f481d26080cdebbfebdcb839de0b7c28a5a8501c098eb632298a3947f5010c039eddda79fad197cd55bda4a93ae0d'}
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
     url=input("enter the url:")
     a=formcount(url)
